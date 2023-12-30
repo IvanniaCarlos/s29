656f0058003fc3a04e7d327d76bbbaf239f02407
ivannia-peruvian <conysashenkapizangoramirez@gmail.com>
2023-12-30 12:14:16 -0500

    proyectounidadIII

--- practica.tec/modulo.js
+++ practica.tec/modulo.js
@@ -1,5 +1,16 @@
 //función que muestra los 10 primeron números
-function  contar(params) {
+function  contar() {
     // for (inicio,final,pasos)
-    for (let i= )
+    for (let i= 1; i <= 10;i++) {
+        console.log(i)
+    }
 }
+
+//Función para saludar con parámetro
+function saludar(nombre) {
+    console.log(`Hola ${nombre}`)
+}
+//PI
+ const PI =3.14159;
+//Exportamos la función//
+export {contar,saludar};
---
+++ practica.tec/practica.js
@@ -0,0 +1,8 @@
+x
+
+
+import {contar} from './modulo.js';
+//llamamos la función//
+contar ();
+saludar("Diana");
+console.log(`El valor de PI es ${PI}`);
